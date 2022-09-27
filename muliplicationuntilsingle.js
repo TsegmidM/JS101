d4a2019b9f6956bfdcf99ba858e0d96a5c9e077f
TsegmidM <tsegmid.education.icodice.com>
2022-09-27 11:37:42 -0500

    change

--- muliplicationuntilsingle.js
+++ muliplicationuntilsingle.js
@@ -1,5 +1,14 @@
+let count = 0;
 const multiplicationToSingle = (n) => {
-  
+    let total = 1;
+    for (let i = 0; i < n.toString().length; i++) {
+        total *= n.toString()[i];
+    }
+    count++;
+    console.log(`Step ${count}: ${total}`);
+
+    if (total < 10) return total;
+    else return multiplicationToSingle(total);
+
 }
 multiplicationToSingle(738);
-
