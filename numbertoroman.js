fc9dd2ecde4ff5d4dbdb80b2a722cf48d8696bbe
TsegmidM <tsegmid.education.icodice.com>
2022-09-29 22:58:40 -0500

    nToR

--- numbertoroman.js
+++ numbertoroman.js
@@ -1,5 +1,28 @@
-const convertToRoman = (n) => {
+const convertToRoman = (num) => {
+    const alphabets = {
+        C: 100,
+        XC: 90,
+        L: 50,
+        XL: 40,
+        X: 10,
+        IX: 9,
+        V: 5,
+        IV: 4,
+        I: 1
+    };
+    console.log(alphabets.C);
+    let string = '';
 
+    for(const key in alphabets){
+        const numberValue = alphabets[key];;
+       // console.log(numberValue);
+        while(numberValue <= num){
+            num -= numberValue;
+            string += key;
+        }
+    }
+    return string;
 }
-convertToRoman(22);
-convertToRoman(39);
+console.log(convertToRoman(22));
+console.log(convertToRoman(49)); 
+console.log(convertToRoman(99)); 
