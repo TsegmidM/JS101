9ec1a2964bf28774251eda4a172203df10c3d29e
TsegmidM <tsegmid.education.icodice.com>
2022-10-04 12:59:05 -0500

    updated

--- sumof45.js
+++ sumof45.js
@@ -2,11 +2,12 @@ const func = (a) => {
     let sum = 0;
     if(a < 0)
     return 0;
-    for(let i = 1; i < a; i++){
+    for(let i = 1; i <= a; i++){
         if(i % 4 === 0 || i % 5 === 0)
         sum += i;
     }   
     return sum;
 }
 console.log(func(11));
+console.log(func(12));
 console.log(func(-1));
